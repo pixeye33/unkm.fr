5270dc8cf1fabdb92990dfc886d10dfe7d045526
Jean-Marie Favreau <jeanmarie.favreau@free.fr>
2021-01-06 19:47:32 +0100

    Consider tag access=private (#15)

--- ressources/loop.js
+++ ressources/loop.js
@@ -763,7 +763,7 @@ function displayCircularPath(e) {
     const square = walkCircle.getBounds();
     const bboxString = square.getSouth() + "," + square.getWest() + ", " + square.getNorth() + "," + square.getEast();
 
-    const query = "[out:json][timeout:25];(way[\"highway\"][\"highway\"!~\"^(motorway|construction|trunk|trunk_link|motorway_link)\"][\"foot\"!~\"^(no|private)\"](" + bboxString + ");way[\"pathway\"](" + bboxString + "););out;>;out skel qt;";
+    const query = "[out:json][timeout:25];(way[\"highway\"][\"highway\"!~\"^(motorway|construction|trunk|trunk_link|motorway_link)\"][\"access\"!~\"^(private)\"][\"foot\"!~\"^(no|private)\"](" + bboxString + ");way[\"pathway\"](" + bboxString + "););out;>;out skel qt;";
 
     const server = "https://overpass.kumi.systems/api/interpreter";
 

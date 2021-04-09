dfa81897e641c2547da086264c562dec2a0bf41d
GarenKreiz <garenkreiz@gmail.com>
2021-04-09 21:06:30 +0200

    Gpx download (#17)
    * Change button from Save to GPX
    
    * Change tab to spaces
    
    * Download GPX for 1km and 10km circles

--- ressources/loop.js
+++ ressources/loop.js
@@ -332,15 +332,92 @@ const gpxFooter = `
 
 var gpxBody = "";
 
-function gpxForLoop() {
-     var gpx = "";
-    
 
-     if (window.ClickCircularPaths.length != 0) { 
-	 gpx += gpxHeader;
-	 gpx += gpxBody;
-	 gpx += gpxFooter;
+/*!
+ * JavaScript function to calculate the destination point given start point latitude / longitude (numeric degrees), bearing (numeric degrees) and distance (in m).
+ *
+ * Taken from http://movable-type.co.uk/scripts/latlong-vincenty-direct.html and optimized / cleaned up by Mathias Bynens <http://mathiasbynens.be/>
+ * Source : https://gist.github.com/mathiasbynens/354587/4137c2350d7cee9b757444ef3a6f13a1f69c2abc
+ * Based on the Vincenty direct formula by T. Vincenty, "Direct and Inverse Solutions of Geodesics on the Ellipsoid with application of nested equations", Survey Review, vol XXII no 176, 1975 <http://www.ngs.noaa.gov/PUBS_LIB/inverse.pdf>
+ */
+
+function toRad(degrees)
+{
+    return degrees * (Math.PI/180);
+}
+function toDeg(radians)
+{
+    return radians * (180 / Math.PI);
+}
+
+function destVincenty(lat1, lon1, brng, dist) {
+    var a = 6378137,
+        b = 6356752.3142,
+        f = 1 / 298.257223563, // WGS-84 ellipsiod
+        s = dist,
+        alpha1 = toRad(brng),
+        sinAlpha1 = Math.sin(alpha1),
+        cosAlpha1 = Math.cos(alpha1),
+        tanU1 = (1 - f) * Math.tan(toRad(lat1)),
+        cosU1 = 1 / Math.sqrt((1 + tanU1 * tanU1)), sinU1 = tanU1 * cosU1,
+        sigma1 = Math.atan2(tanU1, cosAlpha1),
+        sinAlpha = cosU1 * sinAlpha1,
+        cosSqAlpha = 1 - sinAlpha * sinAlpha,
+        uSq = cosSqAlpha * (a * a - b * b) / (b * b),
+        A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq))),
+        B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq))),
+        sigma = s / (b * A),
+        sigmaP = 2 * Math.PI;
+    while (Math.abs(sigma - sigmaP) > 1e-12) {
+        var cos2SigmaM = Math.cos(2*sigma1 + sigma),
+            sinSigma = Math.sin(sigma),
+            cosSigma = Math.cos(sigma),
+            deltaSigma = B * sinSigma * (cos2SigmaM + B / 4 * (cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM) - B / 6 * cos2SigmaM * (-3 + 4 * sinSigma * sinSigma) * (-3 + 4 * cos2SigmaM * cos2SigmaM)));
+            sigmaP = sigma;
+        sigma = s / (b * A) + deltaSigma;
+    };
+    var tmp = sinU1 * sinSigma - cosU1 * cosSigma * cosAlpha1,
+        lat2 = Math.atan2(sinU1 * cosSigma + cosU1 * sinSigma * cosAlpha1, (1 - f) * Math.sqrt(sinAlpha * sinAlpha + tmp * tmp)),
+        lambda = Math.atan2(sinSigma * sinAlpha1, cosU1 * cosSigma - sinU1 * sinSigma * cosAlpha1),
+        C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha)),
+        L = lambda - (1 - C) * f * sinAlpha * (sigma + C * sinSigma * (cos2SigmaM + C * cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM))),
+        revAz = Math.atan2(sinAlpha, -tmp); // final bearing
+    return [toDeg(lat2), lon1 + toDeg(L)];
+};
+
+function gpxOneCircle(lat, lng, radius)
+{
+    var gpxResult = "";
+    var pt = destVincenty(lat,lng,0,radius);
+    gpxResult += '<rtept lat="' + pt[0] + '" lon="' + pt[1] + '" />';
+    for (var d=0; d != 721; ++d)
+    {
+        pt = destVincenty(lat,lng,d*0.5,radius);
+        gpxResult += '<rtept lat="' + pt[0] + '" lon="' + pt[1] + '" />';
+
     }
+    return gpxResult;
+}
+
+function gpxCircles() {
+    var gpxResult = "";
+    gpxResult += "\n<rte><name>Cercle de 1km</name>";
+    gpxResult += gpxOneCircle(window.center.lat, window.center.lng, 1000);
+    gpxResult += "</rte>";
+    gpxResult += "\n<rte><name>Cercle de 10km</name>";
+    gpxResult += gpxOneCircle(window.center.lat, window.center.lng, 10000);
+    gpxResult += "</rte>";
+    return gpxResult;
+}
+function gpxForLoop() {
+    var gpx = "";
+    
+    gpx += gpxHeader;
+    if (window.ClickCircularPaths.length != 0) { 
+       gpx += gpxBody;
+    }
+    gpx += gpxCircles();
+    gpx += gpxFooter;
 
     return gpx;
 }
@@ -781,6 +858,7 @@ function displayCircularPath(e) {
 
         const response = JSON.parse(request.response);
 
+        gpxBody = "";
 
         // get only nodes inside the disc
         var nodes = getNodesInside(response.elements);
@@ -816,63 +894,60 @@ function displayCircularPath(e) {
                 
                 // build a copy of the shortest path
                 var shortestPath = computeShortestPath(fullGraph, graph.nodes);
-                
-
-		gpxBody = "";
 
                 // draw the shortest path and generate GPX
-		if (shortestPath.edges.length)
-		{
-		    gpxBody += "<rte><name>Accès à la boucle</name>";
-		    gpxBody += '<rtept lat="' + shortestPath.nodes[shortestPath.edges[0][0]].lat + '" lon="' + shortestPath.nodes[shortestPath.edges[0][0]].lng + '" />';
-                    for(var e = 0; e != shortestPath.edges.length; ++e) {
-			gpxBody += '<rtept lat="' + shortestPath.nodes[shortestPath.edges[e][1]].lat + '" lon="' + shortestPath.nodes[shortestPath.edges[e][1]].lng + '" />';
-			window.ClickCircularPaths.push(L.polyline(toPolyLine(shortestPath.edges[e], shortestPath.nodes), { color: '#2ab50a'}).addTo(map));
-                    }
-		    gpxBody += "</rte>\n";
-		}
+                if (shortestPath.edges.length)
+                {
+                    gpxBody += "<rte><name>Accès à la boucle</name>";
+                    gpxBody += '<rtept lat="' + shortestPath.nodes[shortestPath.edges[0][0]].lat + '" lon="' + shortestPath.nodes[shortestPath.edges[0][0]].lng + '" />';
+                            for(var e = 0; e != shortestPath.edges.length; ++e) {
+                    gpxBody += '<rtept lat="' + shortestPath.nodes[shortestPath.edges[e][1]].lat + '" lon="' + shortestPath.nodes[shortestPath.edges[e][1]].lng + '" />';
+                    window.ClickCircularPaths.push(L.polyline(toPolyLine(shortestPath.edges[e], shortestPath.nodes), { color: '#2ab50a'}).addTo(map));
+                            }
+                    gpxBody += "</rte>\n";
+                }
 
-		var neighbours = {};
+                var neighbours = {};
 
                 // draw the contour and generate GPX
                 for(var e = 0; e != graph.edges.length; ++e) {
                     window.ClickCircularPaths.push(L.polyline(toPolyLine(graph.edges[e], graph.nodes), { color: '#0060f0'}).addTo(map));
 
-		    if (! neighbours[graph.edges[e][0]]) neighbours[graph.edges[e][0]] = [];
-		    neighbours[graph.edges[e][0]].push([graph.edges[e][1]]);
-		    if (! neighbours[graph.edges[e][1]]) neighbours[graph.edges[e][1]] = [];
-		    neighbours[graph.edges[e][1]].push([graph.edges[e][0]]);
+                    if (! neighbours[graph.edges[e][0]]) neighbours[graph.edges[e][0]] = [];
+                    neighbours[graph.edges[e][0]].push([graph.edges[e][1]]);
+                    if (! neighbours[graph.edges[e][1]]) neighbours[graph.edges[e][1]] = [];
+                    neighbours[graph.edges[e][1]].push([graph.edges[e][0]]);
                 }
 
-		var neighboursVisited = {};
-		var currentNode;
-		var nextNode;
+                var neighboursVisited = {};
+                var currentNode;
+                var nextNode;
 
-		if (shortestPath.edges[0])
-		{
-		    currentNode = shortestPath.edges[0][0];
-		}
-		else
-		{
-		    currentNode = graph.edges[0][0];
-		}
+                if (shortestPath.edges[0])
+                {
+                    currentNode = shortestPath.edges[0][0];
+                }
+                else
+                {
+                    currentNode = graph.edges[0][0];
+                }
 
-		gpxBody += "<rte><name>Boucle de marche</name>";
-		gpxBody += '<rtept lat="' + graph.nodes[currentNode].lat + '" lon="' + graph.nodes[currentNode].lng + '" />';
+                gpxBody += "<rte><name>Boucle de marche</name>";
+                gpxBody += '<rtept lat="' + graph.nodes[currentNode].lat + '" lon="' + graph.nodes[currentNode].lng + '" />';
 
-		nextNode = neighbours[currentNode].pop();
-		while (nextNode)
-		{
-		    gpxBody += '<rtept lat="' + graph.nodes[nextNode].lat + '" lon="' + graph.nodes[nextNode].lng + '" />';
-		    if (! neighboursVisited[currentNode]) neighboursVisited[currentNode] = [];
-		    neighboursVisited[currentNode].push(nextNode);
-		    if (! neighboursVisited[nextNode]) neighboursVisited[nextNode] = [];
-		    neighboursVisited[nextNode].push(currentNode);
-		    currentNode = nextNode;
-		    nextNode = neighbours[currentNode].pop();
-		    while (neighboursVisited[nextNode] && neighboursVisited[nextNode].includes(currentNode)) nextNode = neighbours[currentNode].pop();
-		}
-		gpxBody += "</rte>\n";
+                nextNode = neighbours[currentNode].pop();
+                while (nextNode)
+                {
+                    gpxBody += '<rtept lat="' + graph.nodes[nextNode].lat + '" lon="' + graph.nodes[nextNode].lng + '" />';
+                    if (! neighboursVisited[currentNode]) neighboursVisited[currentNode] = [];
+                    neighboursVisited[currentNode].push(nextNode);
+                    if (! neighboursVisited[nextNode]) neighboursVisited[nextNode] = [];
+                    neighboursVisited[nextNode].push(currentNode);
+                    currentNode = nextNode;
+                    nextNode = neighbours[currentNode].pop();
+                    while (neighboursVisited[nextNode] && neighboursVisited[nextNode].includes(currentNode)) nextNode = neighbours[currentNode].pop();
+                }
+                gpxBody += "</rte>\n";
 
                 setInformationDistance(mainCC.size, computeSize(shortestPath));
             }
